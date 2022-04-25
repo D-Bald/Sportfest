4c650f42d6c6d46404476c7f46028bf250f48b4b
D-Bald <david.baldauf@web.de>
2022-04-25 16:21:45 +0200

    Shell script für schnelleres Deployment

--- create_realease_and_start.sh
+++ create_realease_and_start.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
-# set environment variables
+echo "Setting environment variables"
 export SECRET_KEY_BASE=$(mix phx.gen.secret)
 export DATABASE_URL=ecto://postgres:postgres@localhost/sportfest_prod
 export PHX_HOST=HOSTNAME_ODER_localhost
 export PORT=4001
 
-# load dependencies to compile code and assets:
+echo "Loading dependencies to compile code and assets"
 # initial setup
 mix deps.get --only prod
 MIX_ENV=prod mix compile
+# compile assets
+$ MIX_ENV=prod mix assets.deploy
 
-# build release
+echo "Building release"
 MIX_ENV=prod mix release
 
-# run migrations
+echo "Running migrations"
 _build/prod/rel/sportfest/bin/sportfest eval "Sportfest.Release.migrate"
 
-# run as daemon
+echo "Running app as daemon"
 _build/prod/rel/sportfest/bin/sportfest daemon
