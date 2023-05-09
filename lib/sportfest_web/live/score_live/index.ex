0712ea8530e60d12b66d53cc6fd95b101d07cd71
D-Bald <david.baldauf@web.de>
2023-05-09 19:36:18 +0200

    Temporary Assigns für Scores sparen Speicher und Daten über Websockets pro update.

--- lib/sportfest_web/live/score_live/index.ex
+++ lib/sportfest_web/live/score_live/index.ex
@@ -3,7 +3,6 @@ defmodule SportfestWeb.ScoreLive.Index do
 
   alias Sportfest.Ergebnisse
   alias Sportfest.Vorbereitung
-  alias Sportfest.Utils.ListItems
 
   @impl true
   def mount(_params, session, socket) do
@@ -15,17 +14,20 @@ defmodule SportfestWeb.ScoreLive.Index do
     socket = assign_defaults(session, socket)
               |> assign(:page_title, "Scores")
               |> assign(klassen: Vorbereitung.list_klassen(),
-                        stationen: Vorbereitung.list_stationen(), filter: filter, scores: [],
-                        ausgewählte_station: nil) # Assign aktuelle station, damit die Bedingungen für Medaillen gelesen werden können
+                        stationen: Vorbereitung.list_stationen(),
+                        ausgewählte_station: nil, # Assign aktuelle station, damit die Bedingungen für Medaillen gelesen werden können
+                        filter: filter,
+                        scores: [])
+              |> assign(:reload_id, station_klasse_id_sum(filter))
 
-    {:ok, socket}
+    {:ok, socket, temporary_assigns: [scores: []]}
   end
 
   @impl true
   # reset filters : each col filter is set to "All"
   def handle_event("reset", _, socket) do
     filter =  %{"station_id" => "None", "klasse_id" => "None"}
-    {:noreply, assign(socket, scores: [], filter: filter, ausgewählte_station: nil)}
+    {:noreply, assign(socket, scores: [], filter: filter, ausgewählte_station: nil, reload_id: 0)}
   end
 
   # filter : add new filter to the socket
@@ -34,22 +36,24 @@ defmodule SportfestWeb.ScoreLive.Index do
     filter = Map.delete(filter, "_target") # Keine Ahnung, wo das her kommt, stört aber
 
     new_filter = socket.assigns.filter |> Map.merge(filter)
-    filter_rows = cond do
-      Enum.any?(Map.values(new_filter), &match?("None", &1)) -> []
-      true -> get_filter_rows(new_filter)
+    {filter_rows, ausgewaehlte_station} = cond do
+      Enum.any?(Map.values(new_filter), &match?("None", &1)) -> {[], nil}
+      true -> {get_filter_rows(new_filter), get_selected_station_from_filter(new_filter)}
     end
 
     {:noreply, assign(socket, scores: filter_rows, filter: new_filter,
-                      ausgewählte_station: get_selected_station_from_filter(new_filter))}
+                      ausgewählte_station: ausgewaehlte_station,
+                      reload_id: station_klasse_id_sum(new_filter))}
   end
 
   # handles clicks on different medals
   def handle_event("set_medaille", %{"score_id" => score_id, "medaille" => medaille}, socket) do
     with score = Ergebnisse.get_score!(score_id) do
       case Ergebnisse.update_score(score, %{"medaille" => medaille}) do
+
         {:ok, _score} ->
           {:noreply,
-           socket
+          socket
            |> put_flash(:info, "Medaille erfolgreich hinzugefügt")}
 
         {:error, %Ecto.Changeset{} = changeset} ->
@@ -69,9 +73,7 @@ defmodule SportfestWeb.ScoreLive.Index do
   def handle_info({:score_updated, score}, socket) do
     {:noreply,
            socket
-           |> update(:scores, fn scores ->
-                ListItems.replace_item_by_id_or_add(scores, score)
-                end )}
+           |> update(:scores, fn scores -> [score | scores] end)}
   end
 
   @impl true
@@ -97,6 +99,20 @@ defmodule SportfestWeb.ScoreLive.Index do
       false -> true
       end
   end
+
+  def station_klasse_id_sum(%{"station_id" => "None", "klasse_id" => "None"}) do
+    0
+  end
+  def station_klasse_id_sum(%{"station_id" => station_id, "klasse_id" => "None"}) do
+    String.to_integer(station_id)
+  end
+  def station_klasse_id_sum(%{"station_id" => "None", "klasse_id" => klasse_id}) do
+    String.to_integer(klasse_id)
+  end
+  def station_klasse_id_sum(%{"station_id" => station_id, "klasse_id" => klasse_id}) do
+    String.to_integer(station_id) + String.to_integer(klasse_id)
+  end
+
 
   @doc """
   Holt die im gegebenen `filter` unter `station_id` angegebene Station aus der Datenbank.
@@ -124,6 +140,7 @@ defmodule SportfestWeb.ScoreLive.Index do
       |> Enum.filter(fn score -> not score.station.team_challenge end)
       |> Enum.filter(fn score -> score.schueler.aktiv end)
       |> Enum.sort_by(fn score -> score.schueler.name end, :asc)
+
     ergebnisse_team = Enum.filter(ergebnisse, fn score -> score.station.team_challenge end)
 
     ergebnisse_single_sorted ++ ergebnisse_team
