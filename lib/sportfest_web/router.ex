6062ba8ae7095743199b2fe26990ba8b0085542e
D-Bald <david.baldauf@web.de>
2023-05-19 15:43:05 +0200

    Fly launch

--- lib/sportfest_web/router.ex
+++ lib/sportfest_web/router.ex
@@ -43,6 +43,9 @@ defmodule SportfestWeb.Router do
     live "/stationen/:id/show/edit", StationLive.Show, :edit
 
     post "/export/:resource", ExportController, :create
+
+    import Phoenix.LiveDashboard.Router
+    live_dashboard "/dashboard", metrics: SportfestWeb.Telemetry
   end
 
   scope "/", SportfestWeb do
@@ -85,15 +88,15 @@ defmodule SportfestWeb.Router do
   # If your application does not have an admins-only section yet,
   # you can use Plug.BasicAuth to set up some basic authentication
   # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
+  # if Mix.env() in [:dev, :test] do
+  #   import Phoenix.LiveDashboard.Router
 
-    scope "/" do
-      pipe_through :browser
+  #   scope "/" do
+  #     pipe_through :browser
 
-      live_dashboard "/dashboard", metrics: SportfestWeb.Telemetry
-    end
-  end
+  #     live_dashboard "/dashboard", metrics: SportfestWeb.Telemetry
+  #   end
+  # end
 
   # Enables the Swoosh mailbox preview in development.
   #
---
+++ rel/overlays/bin/migrate.bat
@@ -0,0 +1,1 @@
+call "%~dp0\sportfest" eval Sportfest.Release.migrate
