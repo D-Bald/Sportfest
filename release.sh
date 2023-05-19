94235d7bfa1c419598dc8ac65dfbfd31252628a5
D-Bald <david.baldauf@web.de>
2023-05-19 19:46:40 +0200

    run db preparation for accounts as release_command

--- release.sh
+++ release.sh
@@ -6,3 +6,5 @@ _build/prod/rel/sportfest/bin/sportfest eval "Sportfest.Release.migrate"
 
 # helper function to create admin, moderator and user accounts if needed
 _build/prod/rel/sportfest/bin/sportfest eval "Sportfest.Release.maybe_create_accounts"
+
+# exit 123
